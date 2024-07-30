eff77e7727b24fc575a2317828fede052ef0a22a
nthinh.dev <nthinh.dev@gmail.com>
2024-07-30 16:13:55 +0700

    feat(jwt): add module jwt

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from '@/application/di/app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from '@/presentation/commons/interceptors';
 import {
   HttpExceptionFilter,
@@ -10,6 +10,7 @@ import { TransformInterceptor } from './presentation/commons/interceptors/transf
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('bootstrap');
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -19,10 +20,15 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
+  if (process.env.NODE_ENV !== 'production') {
+    logger.log(`Running on ${process.env.NODE_ENV} mode`);
+  }
   app.useGlobalInterceptors(
     new TransformInterceptor(),
     new ResponseInterceptor(app.get(Reflector)),
   );
+
+  logger.log(`Listening on port ${process.env.PORT}`);
   await app.listen(process.env.PORT);
 }
 bootstrap();
